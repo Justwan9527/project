d53db65cd99da47a8b6cf5a7be87813c24e67e83
Glenn <106247779+Justwan9527@users.noreply.github.com>
2024-03-06 20:39:16 +0800

    Update main.go
    complete SIA

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 
+	"net"
+	"time"
+
 	"github.com/sigurn/crc16"
 )
 
@@ -10,16 +13,8 @@ func main() {
 	//
 	//dataBuff := bytes.NewBuffer([]byte{})
 
-	//length of the message
-	var lLLL int = 64
-
-	//convert 0LLL to 3 hex digits(in ASCII)
-	var sLLL string = fmt.Sprintf("0%03x", lLLL)
-
-	fmt.Println("sLLL:", sLLL)
-
 	//ID Token, SIA-DCS or ADM-CID
-	var id string = "ADM-CID"
+	var id string = "SIA-DCS"
 
 	fmt.Println("id:", id)
 
@@ -29,7 +24,9 @@ func main() {
 	fmt.Println("seq:", seq)
 
 	//Rrcvr. receiver number consists of an ASCII "R", follow by 1-6 HEX ASCII digits.
-	var Rrcvr string
+	var Rrcvr string = "579BD"
+
+	Rrcvr = "R" + Rrcvr
 
 	fmt.Println("Rrcvr:", Rrcvr)
 
@@ -39,28 +36,61 @@ func main() {
 
 	//acct. account number consists of an ASCII "#", followed by 3-16 ASCII characters representing hexadecimal digits
 
-	var acct string = "#1234"
+	var acct string = "Test123"
 	fmt.Println("acct:", acct)
+
+	acct = "#" + acct
 
 	//[data] all data is in ASCII characters and the bracket characters "[" and "]" are included.
 
-	var data string = "[" + acct + "|" + "1140 00 007" + "]"
+	var data string = "[" + acct + "|" + "NF1234/NPAZone123" + "]"
 	fmt.Println("data:", data)
 
 	//timestamp. the format of the timestamp is:<_HH:MM:SS,MM-DD-YYYY>. the braces are not part of the transmitted message. But the dunerscore, colon, commaand hyphen characters are included
-	var timestamp string = "_22:49:34,01-22-2012"
+	var timestamp string = "_16:08:34,03-06-2024"
 	fmt.Println("timestamp:", timestamp)
 
 	var data4crc string = "\"" + id + "\"" + seq + Rrcvr + LPref + acct + data + timestamp
 	fmt.Println("data4crc:", data4crc, "length:", len(data4crc))
 
+	//length of the message
+	var lLLL int = len(data4crc)
+
+	//convert 0LLL to 3 hex digits(in ASCII)
+	var sLLL string = fmt.Sprintf("0%03x", lLLL)
+	fmt.Println("sLLL:", sLLL)
 	//calculate crc
 
 	table := crc16.MakeTable(crc16.CRC16_ARC)
 	crc := crc16.Checksum([]byte(data4crc), table)
-	c := fmt.Sprintf("%x", crc)
+	c := fmt.Sprintf("%X", crc)
 	fmt.Println("CRC16==", c)
 
-	//binary.Write(dataBuff)
+	//complete SIA data
+	var siaData string = "\r" + c + sLLL + data4crc + "\n"
+	fmt.Println("siaData:", siaData)
+
+	//send sia data to platform by listening mode
+	var network string = "tcp"
+	var ip string = "10.7.66.16"
+	var port string = "15646"
+	var address string = ip + ":" + port
+	conn, err := net.DialTimeout(network, address, time.Duration(5)*time.Second)
+	if err != nil {
+		fmt.Println("DialTimeout err:", err)
+		return
+	}
+
+	defer conn.Close()
+
+	nSend, err := conn.Write([]byte(siaData))
+	if err != nil {
+		fmt.Println("Write err:", err)
+		return
+	} else {
+		fmt.Println("Write success, write len:", nSend)
+	}
+
+	time.Sleep(time.Duration(20) * time.Second)
 
 }
